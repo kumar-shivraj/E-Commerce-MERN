910d91e4736b7d4317b2386ab6ea59fa30c332b6
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-04-09 21:10:47 +0530

    frontend: implement react router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,22 +2,23 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import React from "react";
 import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand as={Link} to="/">
             <img src={Logo} alt="ProShop" />
             ProShop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+              <Nav.Link as={Link} to="/cart">
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href="/login">
+              <Nav.Link as={Link} to="/login">
                 <FaUser /> Sign In
               </Nav.Link>
             </Nav>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
