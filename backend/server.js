b4f5a4d6145a6a3ff5bb7f8a320943f340134f28
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-04-10 21:55:16 +0530

    backend: connect with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-const port = process.env.PORT || 8000;
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
+const port = process.env.PORT || 8000;
 
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
