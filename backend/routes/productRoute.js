e15df6f8ac0a5334c743b1110754e70fca1868ed
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-04-12 04:39:16 +0530

    backend: seperate the logic for getting the product inside controllers

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,27 +1,12 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-
-    return res.json(product);
-  })
-);
+router.get("/", getProducts);
+router.get("/:id", getProductById);
 
 export default router;
