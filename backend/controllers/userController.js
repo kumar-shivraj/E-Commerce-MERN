f29736ee366bbf703971b856bbce737e2f584965
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-04-13 10:48:25 +0530

    backend: implement logout user endpoint with clearing the JWT cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,7 +43,13 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/logout
 // @access private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully!" });
 });
 
 // @desc Get user profile
