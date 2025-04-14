76c1a7ff92a888dabec2055cc3e5cabb6edb846e
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-04-15 05:08:31 +0530

    frontend: implement private route and make shipping screen as private as it should be visible only if the user is logged in

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,6 +19,7 @@ import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
 import ShippingScreen from "./screens/ShippingScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,7 +29,9 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/shipping" element={<ShippingScreen />} />
+      <Route path="shipping" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
